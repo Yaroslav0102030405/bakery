31fe6b8f9d2f1cec9eb725760056c7a941755142
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-12-08 20:01:39 +0200

    0

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,3 +9,19 @@ const toggleMenu = () => {
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
