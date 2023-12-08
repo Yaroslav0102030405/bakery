8f5d5c1350666972951b49e52ad387d99e18706e
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-12-08 11:08:51 +0200

    виправлення багу проскутка моделаьного вікна

--- js/modal.js
+++ js/modal.js
@@ -2,8 +2,10 @@ const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModal = () => {
+  document.body.classList.toggle('modal-open');
+  modal.classList.toggle('is-hidden');
+};
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
-
