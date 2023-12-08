93f3c57c12776be2489562430024446ddb42b4d4
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-12-08 16:42:47 +0200

    1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,10 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => {
+  document.body.classList.toggle('modal-open');
+  mobileMenu.classList.toggle('is-open');
+};
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
