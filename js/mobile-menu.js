5e7da3e1730ea057b4664039dd3e97a29ddcaad0
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-01 00:09:36 +0200

    fix mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const mobile = document.querySelector('.mobile');
 
 const toggleMenu = () => {
   document.body.classList.toggle('modal-open');
@@ -9,18 +10,77 @@ const toggleMenu = () => {
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+mobile.addEventListener('click', onCloseLinkClick);
 
-// const addListenerBtn = document.querySelector('.js-add-listener');
-// const removeListenerBtn = document.querySelector('.js-remove-listener');
+function onCloseModal() {
+  document.body.classList.remove('modal-open');
+  mobileMenu.classList.toggle('is-open');
+  // openModal.style = 'display: block';
+  // backdropJs.style = "transform: translateX(-100%)";
+}
 
-// function onOpenModalr() {
-//   addListenerBtn.addEventListener('Click', () => {
-//     addListenerBtn.classList('js-open-modal');
-//   });
+function onCloseLinkClick(event) {
+  if (event.target.nodeName === 'A') {
+    onCloseModal();
+  }
+  // Перевірити на який єлемент натиснули
+  // console.log(event.target.nodeName);
+}
+
+// const openModal = document.querySelector('.menu-btn-open');
+// const closeModal = document.querySelector('.menu-btn-close');
+// // const backdropJs = document.querySelector('.js-menu-backdrop');
+
+// // Зробили клік на ссилку через деоегування подій
+// const link = document.querySelector('.mobile');
+
+// // const link1 = document.querySelector(".link1");
+// // const link2 = document.querySelector(".link2");
+// // const link3 = document.querySelector(".link3");
+// // const link4 = document.querySelector(".link4");
+// // const link = document.querySelector(".hav");
+
+// // const link = document.querySelector('a[href="#home"]');
+
+// // на єлементі вішаємо прослушиваніе подій + функії
+// openModal.addEventListener('click', onOpenModal);
+// closeModal.addEventListener('click', onCloseModal);
+// // backdropJs.addEventListener('click', onBackdropClick);
+
+// link.addEventListener('click', onCloseLinkClick);
+
+// // link1.addEventListener("click", onBackdropClick);
+// // link2.addEventListener("click", onBackdropClick);
+// // link3.addEventListener("click", onBackdropClick);
+// // link4.addEventListener("click", onBackdropClick);
+// // link.addEventListener("click", onBackdropClick);
+
+// // link.addEventListener("click", onLinkClick);
+
+// function onOpenModal() {
+//   document.body.classList.add('modal-open');
+//   backdropJs.classList.toggle('is-open');
+//   // openModal.style = 'display: none';
+//   // backdropJs.style = "transform: translateX(0)";
 // }
 
 // function onCloseModal() {
-//   removeListenerBtn.removeEventListener('Click', () => {
-//     removeListenerBtn.classList('js-open-modal');
-//   });
+//   document.body.classList.remove('modal-open');
+//   backdropJs.classList.toggle('is-open');
+//   // openModal.style = 'display: block';
+//   // backdropJs.style = "transform: translateX(-100%)";
+// }
+
+// // function onBackdropClick(event) {
+// //   if (event.currentTarget === event.target) {
+// //     onCloseModal();
+// //   }
+// // }
+
+// function onCloseLinkClick(event) {
+//   if (event.target.nodeName === 'A') {
+//     onCloseModal();
+//   }
+//   // Перевірити на який єлемент натиснули
+//   // console.log(event.target.nodeName);
 // }
